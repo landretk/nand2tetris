b3ddd6db24d6b6c478f41f56eb3bc75250059874
landretk <landreth.keegan@gmail.com>
2015-10-18 12:50:39 -0600

    Wrote multiplication assembly routine

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,57 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	@R2
+	M=0
+
+	@R0
+	D=M
+	@R1
+	D=D-M
+	@R0_LESS_THAN_R1
+	D;JLT
+	@R1_LESS_THAN_R0
+	0;JEQ
+
+(R1_LESS_THAN_R0) //load R0 into iteration counter
+	@R0
+	D=M
+	@n
+	M=D
+	@R1
+	D=M
+	@i
+	M=D
+	@INIT_LOOP
+	0;JEQ
+
+(R0_LESS_THAN_R1) //load R0 into iteration counter
+	@R1
+	D=M
+	@n
+	M=D
+	@R0
+	D=M
+	@i
+	M=D
+
+(INIT_LOOP)
+	@i
+	MD=M-1
+	@END
+	D;JLT
+(LOOP)
+	@n
+	D=M
+	@R2
+	M=M+D
+	@i
+	MD=M-1
+	@LOOP
+	D;JGT
+	D;JEQ
+
+(END)
+	@END
+	0;JEQ
